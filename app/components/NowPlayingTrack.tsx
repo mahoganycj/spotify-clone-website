56b6a771f33ccdc144656da5bc6dbdeffae441d7
mahogany <cjcalimlim7@gmail.com>
2024-03-24 11:12:38 +0800

    Added Hover State for Track Artist

--- app/components/NowPlayingTrack.tsx
+++ app/components/NowPlayingTrack.tsx
@@ -7,9 +7,9 @@ const NowPlayingTrack = () => {
     <div className='flex flex-row items-center gap-4'>
         <Image className='rounded-sm' height={56} width={56} src={"https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebef138aa4569dba63ee549997/2/en/default"} alt=''/>
         
-        <div>
-            <div className='hover:underline cursor-pointer select-none'>Track Name</div>
-            <div className='hover:underline cursor-pointer select-none'>Track Artist</div>
+        <div className='flex flex-col gap-x-0 justify-start'>
+            <div className='hover:underline cursor-pointer select-none font-semibold text-md'>Track Name</div>
+            <div className='hover:underline cursor-pointer select-none text-neutral-700 text-xs font-semibold hover:text-white'>Track Artist</div>
         </div>
 
         <FiPlusCircle size={24} className="text-neutral-500 hover:scale-110 hover:text-white cursor-pointer"/>
